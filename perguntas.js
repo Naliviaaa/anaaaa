32b2b2049431ff4106e8c0051fe29af765fefb31
Naliviaaa <142615230+Naliviaaa@users.noreply.github.com>
2024-10-07 10:45:20 -0300

    g

---
+++ perguntas.js
@@ -0,0 +1,119 @@
+criacartao(
+'Geografia'
+'Qual e a capital do Japao'
+'Tóquio'
+)
+
+criacartao(
+'Arte'
+'Quem pintou a Mona Lisa?'
+'Leonardo da Vinci'
+)
+
+criacartao(
+'Astronomia'
+'Qual é o planeta mais próximo do Sol?'
+'Mercúrio'
+)
+
+criacartao(
+    'Matemática'
+'O que é um triângulo equilátero?'
+'Um triângulo com todos os lados e ângulos iguais'
+)
+
+criacartao(
+    'Literatura'
+'Quem escreveu "O Pequeno Príncipe"?'
+'Antoine de Saint-Exupéry'
+)
+
+criacartao(
+    'Biologia'
+'Qual é o animal terrestre mais rápido do mundo?'
+'Guepardo'
+)
+
+criacartao(
+    'Geografia'
+'Em que continente está o deserto do Saara?'
+'África'
+)
+
+criacartao(
+    'Astronomia'
+'O que é uma supernova?'
+'Uma explosão estelar que ocorre no final da vida de uma estrela'
+)
+
+criacartao(
+    'Ciência'
+'Quem foi Albert Einstein?'
+'Um físico teórico famoso por desenvolver a teoria da relatividade'
+)
+
+criacartao(
+    'Química'
+'Qual é o símbolo químico do ouro?'
+ 'Au'
+)
+
+criacartao(
+    'História'
+'Quem foi a primeira mulher a ganhar um Prêmio Nobel?'
+ 'Marie Curie'
+)
+
+criacartao(
+    'Economia'
+'Qual é a moeda oficial do Reino Unido?'
+ 'Libra esterlina'
+)
+
+criacartao(
+    'Tecnologia'
+'Em que ano foi lançada a primeira versão do Windows?'
+'1985'
+)
+
+criacartao(
+    'Biologia'
+'O que significa a sigla "DNA"?'
+ 'Ácido desoxirribonucleico'
+)
+
+criacartao(
+    'Geografia'
+'Qual é a capital da Austrália?'
+ 'Canberra'
+)
+
+criacartao(
+    'História'
+'Quem foi o primeiro homem a pisar na Lua?'
+ 'Neil Armstrong'
+)
+
+criacartao(
+    'Ferramentas'
+'O que é uma serra elétrica?'
+' Uma ferramenta motorizada usada para cortar madeira'
+)
+
+criacartao(
+    'Geografia'
+'Qual é o nome da maior floresta tropical do mundo?'
+ 'Floresta Amazônica'
+)
+
+criacartao(
+    'História'
+'Em que ano terminou a Segunda Guerra Mundial?'
+ '1945'
+)
+
+criacartao(
+    'Culinária'
+'Qual é a receita básica para fazer pão?'
+'Farinha, água, fermento e sal'
+)
