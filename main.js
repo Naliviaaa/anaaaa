ae9bc8f38d895c2769cee961d5eeaa5eccf884e9
Naliviaaa <142615230+Naliviaaa@users.noreply.github.com>
2024-10-07 15:59:55 -0300

    j

--- main.js
+++ main.js
@@ -1,4 +1,13 @@
 function criacartao(categoria, pergunta, resposta){
     let container = document.getElementById('conteiner');
-    
+    let cartao = document.getElementById('article')
+    cartao.className = 'cartao';
+    cartao.innerHTML = `<div class="conteudo-cartao">
+        <p>pergunta</p>
+    </div>
+    <div class="resposta-cartao">
+        <p>resposta</p>
+    </div>
+`
+container.appendChild(cartao);
 }
--- perguntas.js
+++ perguntas.js
@@ -1,119 +1,119 @@
 criacartao(
-'Geografia'
-'Qual e a capital do Japao'
+'Geografia',
+'Qual e a capital do Japao',
 'Tóquio'
 )
 
 criacartao(
-'Arte'
-'Quem pintou a Mona Lisa?'
+'Arte',
+'Quem pintou a Mona Lisa?',
 'Leonardo da Vinci'
 )
 
 criacartao(
-'Astronomia'
-'Qual é o planeta mais próximo do Sol?'
+'Astronomia',
+'Qual é o planeta mais próximo do Sol?',
 'Mercúrio'
 )
 
 criacartao(
-    'Matemática'
-'O que é um triângulo equilátero?'
+    'Matemática',
+'O que é um triângulo equilátero?',
 'Um triângulo com todos os lados e ângulos iguais'
 )
 
 criacartao(
-    'Literatura'
-'Quem escreveu "O Pequeno Príncipe"?'
+    'Literatura',
+'Quem escreveu "O Pequeno Príncipe"?',
 'Antoine de Saint-Exupéry'
 )
 
 criacartao(
-    'Biologia'
-'Qual é o animal terrestre mais rápido do mundo?'
+    'Biologia',
+'Qual é o animal terrestre mais rápido do mundo?',
 'Guepardo'
 )
 
 criacartao(
-    'Geografia'
-'Em que continente está o deserto do Saara?'
+    'Geografia',
+'Em que continente está o deserto do Saara?',
 'África'
 )
 
 criacartao(
-    'Astronomia'
-'O que é uma supernova?'
+    'Astronomia',
+'O que é uma supernova?',
 'Uma explosão estelar que ocorre no final da vida de uma estrela'
 )
 
 criacartao(
-    'Ciência'
-'Quem foi Albert Einstein?'
+    'Ciência',
+'Quem foi Albert Einstein?',
 'Um físico teórico famoso por desenvolver a teoria da relatividade'
 )
 
 criacartao(
-    'Química'
-'Qual é o símbolo químico do ouro?'
+    'Química',
+'Qual é o símbolo químico do ouro?',
  'Au'
 )
 
 criacartao(
-    'História'
-'Quem foi a primeira mulher a ganhar um Prêmio Nobel?'
+    'História',
+'Quem foi a primeira mulher a ganhar um Prêmio Nobel?',
  'Marie Curie'
 )
 
 criacartao(
-    'Economia'
-'Qual é a moeda oficial do Reino Unido?'
+    'Economia',
+'Qual é a moeda oficial do Reino Unido?',
  'Libra esterlina'
 )
 
 criacartao(
-    'Tecnologia'
-'Em que ano foi lançada a primeira versão do Windows?'
+    'Tecnologia',
+'Em que ano foi lançada a primeira versão do Windows?',
 '1985'
 )
 
 criacartao(
-    'Biologia'
-'O que significa a sigla "DNA"?'
+    'Biologia',
+'O que significa a sigla "DNA"?',
  'Ácido desoxirribonucleico'
 )
 
 criacartao(
-    'Geografia'
-'Qual é a capital da Austrália?'
+    'Geografia',
+'Qual é a capital da Austrália?',
  'Canberra'
 )
 
 criacartao(
-    'História'
-'Quem foi o primeiro homem a pisar na Lua?'
+    'História',
+'Quem foi o primeiro homem a pisar na Lua?',
  'Neil Armstrong'
 )
 
 criacartao(
-    'Ferramentas'
-'O que é uma serra elétrica?'
+    'Ferramentas',
+'O que é uma serra elétrica?',
 ' Uma ferramenta motorizada usada para cortar madeira'
 )
 
 criacartao(
-    'Geografia'
-'Qual é o nome da maior floresta tropical do mundo?'
+    'Geografia',
+'Qual é o nome da maior floresta tropical do mundo?',
  'Floresta Amazônica'
 )
 
 criacartao(
-    'História'
-'Em que ano terminou a Segunda Guerra Mundial?'
+    'História',
+'Em que ano terminou a Segunda Guerra Mundial?',
  '1945'
 )
 
 criacartao(
-    'Culinária'
-'Qual é a receita básica para fazer pão?'
+    'Culinária',
+'Qual é a receita básica para fazer pão?',
 'Farinha, água, fermento e sal'
 )
