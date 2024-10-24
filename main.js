5dcc9e38cd7f9c8060ba60e377453c361480e51e
Naliviaaa <142615230+Naliviaaa@users.noreply.github.com>
2024-10-24 08:46:00 -0300

    m

--- main.js
+++ main.js
@@ -1,16 +1,17 @@
-function criacartao(categoria, pergunta, resposta){
+function criacartao(categoria, pergunta, resposta) {
     let container = document.getElementById('conteiner');
     let cartao = document.getElementById('article')
     cartao.className = 'cartao';
-    cartao.innerHTML =   
-    <div class="conteudo-cartao">
+    cartao.innerHTML = `
+        < div class="conteudo-cartao" >
        <h3>${categoria}</h3>
     <div class="resposta-cartao">
-        <p>${pergunta}</p>
+        <p>${perguntas}</p>
     </div>
     <div class="resposta-cartao">
-        <p>${resposta}</p>
+        <p>${respostas}</p>
     </div>
-</div>
+</div >
+        `
     container.appendChild(cartao);
 }
