21a27f9470307d7aaec94a0f82ac2e6e2152edb1
Naliviaaa <142615230+Naliviaaa@users.noreply.github.com>
2024-11-07 08:46:26 -0300

    j

--- pagina2.js
+++ pagina2.js
@@ -15,13 +15,3 @@ function criacartao(categoria, pergunta, resposta) {
     `
     container.appendChild(cartao);
 }
-
-function mostraResposta (card){
-    const resposta = card.querySelector('.resposta-cartao');
-        if(resposta.style.display === 'none'){
-            resposta.style.display = 'block'
-        }
-        else{
-            resposta.style.display = 'none'
-        }
-}
